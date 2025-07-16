ed69de330ff743a9416d5002732bc8cb56b130e9
soukhya28 <soukhya468@gmail.com>
2025-07-16 11:02:19 +0000

    first-commit

--- resource.tf
+++ resource.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "exerciseterra" {
     location = "East US"
 }
  
-resource "azurerm_storage_account" "souterra" {
+resource "azurerm_storage_account" "nevuriterra" {
     name                     = "terrastoragesounevuri2806"
     resource_group_name      = azurerm_resource_group.exerciseterra.name
     location                 = azurerm_resource_group.exerciseterra.location
